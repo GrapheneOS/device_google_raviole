65184a33a8e4154af1b777c045b2c70214390982
Jack Wu <wjack@google.com>
2019-01-17 21:38:27 +0800

    battery: raviole: disable wireless for pts battery test
    Disable wireless charging during drain, usb charging,
    and charger stress test
      - vendor.disable.wlc 1
    If device put onto dreamliner and shutdwon(drain out),
    it would boot up by DC_CHG and keep shutdown(0%)-boot up
    (by DC_CHG) continously. To avoid this symptom, limit wireless
    current_max as 75000 after device boot up
      - persist.vendor.limit.wlc.current=1
    
    How to verify:
      adb shell setprop vendor.disable.wlc 1
      adb shell cat /sys/class/power_supply/wireless/online >>>> 0
    
      adb shell setprop persist.vendor.limit.wlc.current 1
      adb shell cat /sys/class/power_supply/dc/current_max >>>> 75000
    
    Bug: 179094447
    Test: set the property manually and query the result from file node.
    Change-Id: Ic5791c53f735caa19c73bae750651d774c0d4615
    Signed-off-by: Ken Tsou <kentsou@google.com>
    (synced from commit 84d73dafb7e44bd195adcc4cbb16bad28728aedc)

--- device-oriole.mk
+++ device-oriole.mk
@@ -95,3 +95,9 @@ PRODUCT_PRODUCT_PROPERTIES += ro.com.google.ime.kb_pad_port_b=10
 
 # Display LBE
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += vendor.display.lbe.supported=1
+
+# userdebug specific
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+    PRODUCT_COPY_FILES += \
+        device/google/gs101/init.hardware.wlc.rc.userdebug:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.$(PRODUCT_PLATFORM).wlc.rc
+endif
