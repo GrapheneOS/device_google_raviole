2d185271955ee9c816d0fe6d568195fc16b396d5
linpeter <linpeter@google.com>
2021-08-13 18:46:54 +0800

    display: disable hbm dimming
    Bug: 194656170
    test: no panel dimming
    Change-Id: I5907697c546bd11d6e8ca67b4fde2a67b3ab8764

--- device-raven.mk
+++ device-raven.mk
@@ -90,7 +90,7 @@ PRODUCT_COPY_FILES += \
 	device/google/raviole/raven/display_colordata_dev_cal0.pb:$(TARGET_COPY_OUT_VENDOR)/etc/display_colordata_dev_cal0.pb
 
 #config of display brightness dimming
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += vendor.display.brightness.dimming.usage=1
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += vendor.display.brightness.dimming.usage=2
 
 # NFC
 PRODUCT_COPY_FILES += \
