cf9f57fe8b910a10cd2a4f3e18de52926e265030
Robin Peng <robinpeng@google.com>
2021-11-30 20:53:27 +0800

    Update SVN to 10 for O6R4 December QPR 2021
    Bug: 208388283
    Change-Id: I68825bf96c8b94dc63ebc096c57b9f7c680be090
    (cherry picked from commit 43ce3a4014e13b67b16638899cf5008813bd773c)
    Merged-In:I68825bf96c8b94dc63ebc096c57b9f7c680be090

--- device-raven.mk
+++ device-raven.mk
@@ -145,7 +145,7 @@ endif
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=9
+    ro.vendor.build.svn=10
 
 # Hide cutout overlays
 PRODUCT_PACKAGES += \
