3a897fe9fe8e339293420e4723b156dffbf49348
Robin Peng <robinpeng@google.com>
2021-12-01 11:54:54 +0800

    Update SVN to 11 for O6R4 December QPR 2021
    Bug: 208388283
    Change-Id: I85ff3f78d4465d946cc692b69e4b7e9f916f1c78
    (cherry picked from commit 586a87e5dc4648cfcdf2bec7cf5d160f062a8ba2)
    Merged-In:I85ff3f78d4465d946cc692b69e4b7e9f916f1c78

--- device-oriole.mk
+++ device-oriole.mk
@@ -144,7 +144,7 @@ endif
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=10
+    ro.vendor.build.svn=11
 
 # Hide cutout overlays
 PRODUCT_PACKAGES += \
