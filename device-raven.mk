836db9082621aef4f3227703f94534664916564b
Robin Peng <robinpeng@google.com>
2021-12-15 17:53:48 +0800

    Update SVN to 21 for O6R4 Feb MPR 2022
    Bug: 208388283
    Change-Id: Ibed22f512bb16e2166b81a783a707d0018aa88a3
    (cherry picked from commit 0d1967598533d77996ff363d9eeea35914f72410)
    Merged-In:Ibed22f512bb16e2166b81a783a707d0018aa88a3

--- device-raven.mk
+++ device-raven.mk
@@ -183,7 +183,7 @@ endif
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=20
+    ro.vendor.build.svn=21
 
 # Hide cutout overlays
 PRODUCT_PACKAGES += \
