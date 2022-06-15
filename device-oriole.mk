68c76ff28927bfc99b458ad46ae31806c1ad16b9
Bassem Khalife <bkhalife@google.com>
2022-06-15 23:01:57 +0000

    Update oriole SVN to 32
    Bug: 232157623
    Change-Id: I42923ef9fcdc6879b01ad6ec468f5035b9055dbb

--- device-oriole.mk
+++ device-oriole.mk
@@ -175,7 +175,7 @@ endif
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=31
+    ro.vendor.build.svn=32
 
 # Hide cutout overlays
 PRODUCT_PACKAGES += \
