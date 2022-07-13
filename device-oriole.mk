141a50e5091a1598f6f74cb2bd3451ef0f8de6fe
Bassem Khalife <bkhalife@google.com>
2022-07-13 23:45:51 +0000

    Update oriole SVN to 33
    Bug: 232157623
    Change-Id: I57a53ccd46e0bed37f1778f6dd834893c89d145c
    (cherry picked from commit 510d08ff177cab13eeaba9e976a1c321c7f747d9)
    Merged-In: I57a53ccd46e0bed37f1778f6dd834893c89d145c

--- device-oriole.mk
+++ device-oriole.mk
@@ -168,7 +168,7 @@ endif
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=32
+    ro.vendor.build.svn=33
 
 # Set support hide display cutout feature
 PRODUCT_PRODUCT_PROPERTIES += \
