6e05ed0e45a349191bc51d1d47aa8afd31282cd9
Adam Shih <adamshih@google.com>
2022-08-16 13:56:53 +0800

    move bcmbt settings to gs-common
    Bug: 242661555
    Test: build pass
    Change-Id: I5d1a1e6a0ba11009c18d3d9740042b1c67d35c9d

--- device-raven.mk
+++ device-raven.mk
@@ -32,7 +32,7 @@ include device/google/raviole/audio/raven/audio-tables.mk
 include device/google/gs101/device-shipping-common.mk
 include device/google/gs101/fingerprint/udfps_common.mk
 include device/google/gs101/telephony/pktrouter.mk
-include device/google/gs101/bluetooth/bluetooth.mk
+include device/google/gs-common/bcmbt/bluetooth.mk
 
 ifeq ($(filter factory_raven, $(TARGET_PRODUCT)),)
 include device/google/gs101/fingerprint/udfps_shipping.mk
