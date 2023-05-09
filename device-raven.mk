292e9877e87b53080d643e991c391ad3e3b436d3
Jiaming Liu <jiamingliu@google.com>
2023-05-09 05:00:11 +0000

    Disable WM Extensions on Pixel 6
    Bug: 266196089
    Test: Build
    Change-Id: Id317b1626a522f6b7b8a4926c4bdf21ed67bd765
    (cherry picked from commit e81ae9f5e14ae9f3d87ac9bfa2456d69edf86d92)

--- device-raven.mk
+++ device-raven.mk
@@ -296,13 +296,6 @@ else
 		device/google/raviole/location/gps_user.xml.raven:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.xml
 endif
 
-# Window Extensions
-$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
-
-# Disable Settings large-screen optimization enabled by Window Extensions
-PRODUCT_SYSTEM_PROPERTIES += \
-    persist.settings.large_screen_opt.enabled=false
-
 # Enable DeviceAsWebcam support
 PRODUCT_VENDOR_PROPERTIES += \
     ro.usb.uvc.enabled=true
