21db4433f7797b63775f3ba51956a53e2bca0811
Roshan Pius <rpius@google.com>
2023-12-01 19:19:56 +0000

    Revert^2 "raviole(nfc): Modify NFC overlays for NFC apex"
    35ad1b0498c147fdb6d6003dca5c09b9dd58a73f
    
    Bug: 303286040
    Test: Compiles
    
    Change-Id: I0191f8f24a2dc028a0347766c057aefdc142ed3c

--- device-raven.mk
+++ device-raven.mk
@@ -154,7 +154,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
 	$(RELEASE_PACKAGE_NFC_STACK) \
 	Tag \
-	android.hardware.nfc-service.st
+	android.hardware.nfc-service.st \
+	NfcOverlayRaven
 
 # SecureElement
 PRODUCT_PACKAGES += \
