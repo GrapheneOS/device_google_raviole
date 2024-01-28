74a66f372d5f5c9b4c6cc6eb47231ed0079624dd
Ankur Bakshi <ankurbakshi@google.com>
2024-01-28 00:24:59 +0000

    Update oriole SVN to 77
    Bug: 313797924
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:101091c6f70e778e80d2246dc3e89aa8a83d98e3)
    Merged-In: Ie1289324007b51b1a1f5c58a261da8ed303781ff
    Change-Id: Ie1289324007b51b1a1f5c58a261da8ed303781ff

--- device-oriole.mk
+++ device-oriole.mk
@@ -199,7 +199,7 @@ endif
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=76
+    ro.vendor.build.svn=77
 
 # Set support hide display cutout feature
 PRODUCT_PRODUCT_PROPERTIES += \
