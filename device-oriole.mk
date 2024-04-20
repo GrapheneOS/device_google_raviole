de6ea64e40525a0271e752caa0d1ebdf048857c0
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-04-20 15:33:53 +0300

    remove ro.vendor.build.svn sysprop
    Value of ro.vendor.build.svn in stock OS image used by adevtool might not match the value specified
    here, e.g. when base AOSP tag doesn't match the tag that was used for stock OS build.
    
    adevtool automatically adds missing properties, i.e. removal of ro.vendor.build.svn from here means
    that ro.vendor.build.svn sysprop value from stock OS image will be used instead.

--- device-oriole.mk
+++ device-oriole.mk
@@ -321,3 +321,5 @@ PRODUCT_VENDOR_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.quick_start.oem_id=00e0 \
     ro.quick_start.device_id=oriole
+
+PRODUCT_VENDOR_PROPERTIES := $(filter-out ro.vendor.build.svn=% , $(PRODUCT_VENDOR_PROPERTIES))
