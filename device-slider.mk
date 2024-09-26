1ea54293900eae55e8daa6efe3152debfea1f3dc
Brian Norris <briannorris@google.com>
2024-09-26 12:18:13 -0700

    raviole: Pull init.insmod.*.cfg from vendor_dlkm
    We've started pushing init.insmod.*.cfg into the kernel prebuilts, and
    installing it to /vendor_dlkm/etc, but it may not be available on all
    prebuilt variants (e.g., kernel 5.10).
    
    Here, we:
    1. conditionally install our common init.*cfg to /vendor_dlkm (only if
       $(TARGET_KERNEL_DIR) doesn't have a copy)
    2. point init at /vendor_dlkm/etc
    
    When we've cycled completely off of kernel 5.10, we can drop #1.
    
    Bug: 369686096
    Test: build, boot
    Flag: EXEMPT refactor
    Change-Id: Ibefafbcfe1fdf9b4e65ee3d73126c33f04ee193b

--- device-slider.mk
+++ device-slider.mk
@@ -48,9 +48,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	device/google/gs101/conf/init.recovery.device.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.slider.rc
 
-# insmod files
+# insmod files. Kernel 5.10 prebuilts don't provide these yet, so provide our
+# own copy if they're not in the prebuilts.
+# TODO(b/369686096): drop this when 5.10 is gone.
+ifeq ($(wildcard $(TARGET_KERNEL_DIR)/init.insmod.*.cfg),)
 PRODUCT_COPY_FILES += \
-	device/google/raviole/init.insmod.slider.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/init.insmod.slider.cfg
+	device/google/raviole/init.insmod.slider.cfg:$(TARGET_COPY_OUT_VENDOR_DLKM)/etc/init.insmod.slider.cfg
+endif
 
 # Thermal Config
 PRODUCT_COPY_FILES += \
