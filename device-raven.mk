013069b4781ce6a91e69aed75587e01c2250aded
Eileen Lai <eileenisgood@google.com>
2024-10-09 08:13:38 +0000

    modem_svc: use shared_modem_platform to replace all modem_svc_sit
    Bug: 368257019
    
    Flag: EXEMPT resource only update
    Change-Id: I27053324a5b58fa290c3c1a3e5211812bf1acc58

--- device-raven.mk
+++ device-raven.mk
@@ -164,8 +164,11 @@ PRODUCT_PACKAGES += \
 	android.hardware.nfc-service.st \
 	NfcOverlayRaven
 
-# modem_svc_sit daemon
-PRODUCT_PACKAGES += modem_svc_sit
+# Shared Modem Platform
+SHARED_MODEM_PLATFORM_VENDOR := lassen
+
+# Shared Modem Platform
+include device/google/gs-common/modem/shared_modem_platform/shared_modem_platform.mk
 
 # SecureElement
 PRODUCT_PACKAGES += \
--- device-slider.mk
+++ device-slider.mk
@@ -95,8 +95,11 @@ PRODUCT_PACKAGES += \
 	Tag \
 	android.hardware.nfc-service.st
 
-# modem_svc_sit daemon
-PRODUCT_PACKAGES += modem_svc_sit
+# Shared Modem Platform
+SHARED_MODEM_PLATFORM_VENDOR := lassen
+
+# Shared Modem Platform
+include device/google/gs-common/modem/shared_modem_platform/shared_modem_platform.mk
 
 # SecureElement
 PRODUCT_PACKAGES += \
