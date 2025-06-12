df47ac7a44a24ccba5364cf3b512f444dcb19a83
flawedworld <flawedworld@flawed.world>
2025-06-12 19:20:36 +0000

    edit device makefiles to reflect removal of init.hardware.wlc.rc.userdebug in gs101
    Change-Id: If3322ba13ccaa9935698a9007904c4df5e2a46fb

--- device-oriole.mk
+++ device-oriole.mk
@@ -202,12 +202,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.set_idle_timer_ms=1500
 # Media Performance Class 12
 PRODUCT_PROPERTY_OVERRIDES += ro.odm.build.media_performance_class=31
 
-# userdebug specific
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-    PRODUCT_COPY_FILES += \
-        device/google/gs101/init.hardware.wlc.rc.userdebug:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.wlc.rc
-endif
-
 # Increment the SVN for any official public releases
 ifdef RELEASE_SVN_ORIOLE
 TARGET_SVN ?= $(RELEASE_SVN_ORIOLE)
