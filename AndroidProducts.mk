23b72f3f674119b442f04a79b6a13db75b00d16b
Fred Underwood <underwoodfred@proton.me>
2025-06-20 08:49:12 +1000

    rm factory_raven.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -26,8 +26,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/haiku.mk \
     $(LOCAL_DIR)/aosp_raven_pgagnostic_64.mk \
     $(LOCAL_DIR)/aosp_raven_pkvm.mk \
-    $(LOCAL_DIR)/aosp_raven_64.mk \
-    $(LOCAL_DIR)/factory_raven.mk
+    $(LOCAL_DIR)/aosp_raven_64.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_slider-trunk_staging-userdebug \
