c67959ff0d0f156fb6ae82a7dbf04417fd41c67f
Fred Underwood <underwoodfred@proton.me>
2025-06-20 08:46:00 +1000

    rm aosp_whitefin_64.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -34,8 +34,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_slider.mk \
     $(LOCAL_DIR)/aosp_whitefin.mk \
     $(LOCAL_DIR)/aosp_whitefin_pkvm.mk \
-    $(LOCAL_DIR)/aosp_whitefin_hwasan.mk \
-    $(LOCAL_DIR)/aosp_whitefin_64.mk
+    $(LOCAL_DIR)/aosp_whitefin_hwasan.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_slider-trunk_staging-userdebug \
