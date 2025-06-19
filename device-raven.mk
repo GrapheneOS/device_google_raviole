b4fbe7f21447bd8c76214f58875aa44ea302ac00
Fred Underwood <underwoodfred@proton.me>
2025-06-20 09:54:31 +1000

    rm raven build properties

--- device-raven.mk
+++ device-raven.mk
@@ -136,33 +136,10 @@ ACTUATOR_MODEL := luxshare_ict_081545
 PRODUCT_SOONG_NAMESPACES += \
     device/google/raviole
 
-# Increment the SVN for any official public releases
-ifdef RELEASE_SVN_RAVEN
-TARGET_SVN ?= $(RELEASE_SVN_RAVEN)
-else
-# Set this for older releases that don't use build flag
-TARGET_SVN ?= 86
-endif
-
-PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=$(TARGET_SVN)
-
-# Set device family property for SMR
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.device_family=O6R4B9
-
 # Set build properties for SMR builds
 ifeq ($(RELEASE_IS_SMR), true)
     ifneq (,$(RELEASE_BASE_OS_RAVEN))
         PRODUCT_BASE_OS := $(RELEASE_BASE_OS_RAVEN)
-    endif
-endif
-
-# Set build properties for EMR builds
-ifeq ($(RELEASE_IS_EMR), true)
-    ifneq (,$(RELEASE_BASE_OS_RAVEN))
-        PRODUCT_PROPERTY_OVERRIDES += \
-        ro.build.version.emergency_base_os=$(RELEASE_BASE_OS_RAVEN)
     endif
 endif
 
