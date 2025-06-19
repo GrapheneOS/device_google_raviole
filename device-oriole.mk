f98fb99930e99a2e2fa615b92d9be46921cb64f9
Fred Underwood <underwoodfred@proton.me>
2025-06-20 09:38:58 +1000

    rm oriole unavailable soong namespace

--- device-oriole.mk
+++ device-oriole.mk
@@ -150,9 +150,6 @@ PRODUCT_PACKAGES += \
     SettingsOverlayGB7N6 \
     SettingsOverlayG9S9B
 
-# Trusty liboemcrypto.so
-PRODUCT_SOONG_NAMESPACES += vendor/google_devices/raviole/prebuilts
-
 # Set support one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.support_one_handed_mode=true
