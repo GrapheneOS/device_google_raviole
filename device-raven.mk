9ecbbbd686c4b912f8e67c6ea758b6c4da910c2b
Fred Underwood <underwoodfred@proton.me>
2025-06-20 09:53:06 +1000

    rm raven media performance property

--- device-raven.mk
+++ device-raven.mk
@@ -132,9 +132,6 @@ DEVICE_MANIFEST_FILE += \
 
 ACTUATOR_MODEL := luxshare_ict_081545
 
-# Media Performance Class 12
-PRODUCT_PROPERTY_OVERRIDES += ro.odm.build.media_performance_class=31
-
 # PowerStats HAL
 PRODUCT_SOONG_NAMESPACES += \
     device/google/raviole/powerstats/raven \
