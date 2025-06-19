1f4a146ba11ed1a567d7bf16bfbb1c2a638a5865
Fred Underwood <underwoodfred@proton.me>
2025-06-20 08:51:47 +1000

    rm factory_oriole.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -20,7 +20,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_oriole_pgagnostic_64.mk \
     $(LOCAL_DIR)/aosp_oriole_pkvm.mk \
     $(LOCAL_DIR)/aosp_oriole_64.mk \
-    $(LOCAL_DIR)/factory_oriole.mk \
     $(LOCAL_DIR)/aosp_raven.mk
 
 COMMON_LUNCH_CHOICES := \
