12c67054cd23f45911175885359475ce3268d27f
Fred Underwood <underwoodfred@proton.me>
2025-06-20 09:53:49 +1000

    rm powerstats/raven

--- device-raven.mk
+++ device-raven.mk
@@ -134,9 +134,7 @@ ACTUATOR_MODEL := luxshare_ict_081545
 
 # PowerStats HAL
 PRODUCT_SOONG_NAMESPACES += \
-    device/google/raviole/powerstats/raven \
     device/google/raviole
-
 
 # userdebug specific
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
