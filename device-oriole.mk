075c5cc415957bcaa9d84a91f6f9b6ac348f4661
Fred Underwood <underwoodfred@proton.me>
2025-06-20 09:40:58 +1000

    rm oriole RKPD property

--- device-oriole.mk
+++ device-oriole.mk
@@ -162,10 +162,6 @@ PRODUCT_PACKAGES += \
     SettingsOverlayGB7N6 \
     SettingsOverlayG9S9B
 
-# RKPD
-PRODUCT_PRODUCT_PROPERTIES += \
-    remote_provisioning.hostname=remoteprovisioning.googleapis.com \
-
 # Set zram size
 PRODUCT_VENDOR_PROPERTIES += \
     vendor.zram.size=3g
