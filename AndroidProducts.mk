3bb89ae571dbb52fad4f6762898a2ed43890ea3e
Fred Underwood <underwoodfred@proton.me>
2025-06-20 08:48:51 +1000

    rm aosp_slider.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -27,8 +27,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_raven_pgagnostic_64.mk \
     $(LOCAL_DIR)/aosp_raven_pkvm.mk \
     $(LOCAL_DIR)/aosp_raven_64.mk \
-    $(LOCAL_DIR)/factory_raven.mk \
-    $(LOCAL_DIR)/aosp_slider.mk
+    $(LOCAL_DIR)/factory_raven.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_slider-trunk_staging-userdebug \
