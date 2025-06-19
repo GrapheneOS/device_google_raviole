61f7172403dbb0e33ad8771a0563af1847a29f41
Fred Underwood <underwoodfred@proton.me>
2025-06-20 09:37:07 +1000

    rm oriole build properties

--- device-oriole.mk
+++ device-oriole.mk
@@ -128,33 +128,10 @@ ACTUATOR_MODEL := luxshare_ict_081545
 PRODUCT_SOONG_NAMESPACES += \
     device/google/raviole
 
-# Increment the SVN for any official public releases
-ifdef RELEASE_SVN_ORIOLE
-TARGET_SVN ?= $(RELEASE_SVN_ORIOLE)
-else
-# Set this for older releases that don't use build flag
-TARGET_SVN ?= 86
-endif
-
-PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=$(TARGET_SVN)
-
-# Set device family property for SMR builds
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.device_family=O6R4B9
-
 # Set build properties for SMR builds
 ifeq ($(RELEASE_IS_SMR), true)
     ifneq (,$(RELEASE_BASE_OS_ORIOLE))
         PRODUCT_BASE_OS := $(RELEASE_BASE_OS_ORIOLE)
-    endif
-endif
-
-# Set build properties for EMR builds
-ifeq ($(RELEASE_IS_EMR), true)
-    ifneq (,$(RELEASE_BASE_OS_ORIOLE))
-        PRODUCT_PROPERTY_OVERRIDES += \
-        ro.build.version.emergency_base_os=$(RELEASE_BASE_OS_ORIOLE)
     endif
 endif
 
