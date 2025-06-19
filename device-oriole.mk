ec34a545da251af6f90de5beb6f581864d8752d8
Fred Underwood <underwoodfred@proton.me>
2025-06-20 09:22:17 +1000

    rm media_profiles_oriole.xml

--- device-oriole.mk
+++ device-oriole.mk
@@ -93,10 +93,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.bluetooth.le_audio_test=false
 endif
 
-# Camera
-PRODUCT_COPY_FILES += \
-	device/google/raviole/media_profiles_oriole.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
-
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.fixed_fps_range_boost=1
 
