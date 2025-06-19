8e13b8c3cbf33a8c8095e69c4ca2f9c5fafd0bac
Fred Underwood <underwoodfred@proton.me>
2025-06-20 09:35:06 +1000

    rm oriole reference to gs101/init.hardware.wlc.rc.userdebug

--- device-oriole.mk
+++ device-oriole.mk
@@ -128,12 +128,6 @@ ACTUATOR_MODEL := luxshare_ict_081545
 PRODUCT_SOONG_NAMESPACES += \
     device/google/raviole
 
-# userdebug specific
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-    PRODUCT_COPY_FILES += \
-        device/google/gs101/init.hardware.wlc.rc.userdebug:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.wlc.rc
-endif
-
 # Increment the SVN for any official public releases
 ifdef RELEASE_SVN_ORIOLE
 TARGET_SVN ?= $(RELEASE_SVN_ORIOLE)
