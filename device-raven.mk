95a02d682355eb106b5102412096cb4bb410ffdc
Fred Underwood <underwoodfred@proton.me>
2025-06-20 09:46:04 +1000

    remove raven surfaceflinger properties

--- device-raven.mk
+++ device-raven.mk
@@ -65,9 +65,6 @@ $(call soong_config_set,lyric,camera_hardware,raven)
 $(call soong_config_set,lyric,tuning_product,raven)
 $(call soong_config_set,google3a_config,target_device,raven)
 
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.support_kernel_idle_timer=true
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.enable_frame_rate_override=true
-
 # Init files
 PRODUCT_COPY_FILES += \
 	device/google/raviole/conf/init.raven.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.raven.rc
