3638cd9c11505734b11b8462c09090dad6804d48
Fred Underwood <underwoodfred@proton.me>
2025-06-20 09:43:52 +1000

    rm oriole quick_start properties

--- device-oriole.mk
+++ device-oriole.mk
@@ -176,11 +176,6 @@ else
     endif
 endif
 
-# Quick Start device-specific settings
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.quick_start.oem_id=00e0 \
-    ro.quick_start.device_id=oriole
-
 # Disable AVF Remote Attestation
 PRODUCT_AVF_REMOTE_ATTESTATION_DISABLED := true
 
