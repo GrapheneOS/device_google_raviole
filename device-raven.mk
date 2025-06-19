850b852fa4a8c21510f81d1545e15f6d8625ea36
Fred Underwood <underwoodfred@proton.me>
2025-06-20 09:55:32 +1000

    rm raven ime properties

--- device-raven.mk
+++ device-raven.mk
@@ -164,10 +164,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     DeviceAsWebcamRaven
 
-# Keyboard side padding in dp for portrait mode
-PRODUCT_PRODUCT_PROPERTIES += ro.com.google.ime.kb_pad_port_r=11
-PRODUCT_PRODUCT_PROPERTIES += ro.com.google.ime.kb_pad_port_l=11
-
 # DCK properties based on target
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.gms.dck.eligible_wcc=3 \
