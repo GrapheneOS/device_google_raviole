7395f4fc2a42f72d1767c58914a3f993adccb00e
Fred Underwood <underwoodfred@proton.me>
2025-06-20 10:06:19 +1000

    rm audio/raven/

--- device-raven.mk
+++ device-raven.mk
@@ -41,7 +41,6 @@ $(call inherit-product-if-exists, vendor/google_devices/raviole/proprietary/Wall
 
 DEVICE_PACKAGE_OVERLAYS += device/google/raviole/raven/overlay
 
-include device/google/raviole/audio/raven/audio-tables.mk
 include device/google/gs101/device-shipping-common.mk
 include device/google/gs101/telephony/pktrouter.mk
 include device/google/gs-common/bcmbt/bluetooth.mk
